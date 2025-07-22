db11b6aff9b5ea04a482442277fd0ef3e614da48
Edifing <jaimeluciano2003@gmail.com>
2025-07-22 16:49:58 -0500

    Create ecosystem.config.js

---
+++ packages/server/ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'main-app',
+      script: './bin/run',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/app-error.log',
+      out_file: './logs/app-out.log',
+      log_file: './logs/app.log',
+      time: true
+    },
+    {
+      name: 'keepalive',
+      script: './keepalive.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '100M',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/keepalive-error.log',
+      out_file: './logs/keepalive-out.log',
+      log_file: './logs/keepalive.log',
+      time: true
+    }
+  ]
+};
